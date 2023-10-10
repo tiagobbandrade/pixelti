5d48373a4af39a6a0be7278955c17e4995ced839
tiagobbandrade <tiagobbandrade2704@gmail.com>
2023-10-10 14:56:14 -0300

    feat: add search gateway feature

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,9 +1,13 @@
+import { useSearchContext } from '@/contexts/SearchContext'
 import { BiSearch } from 'react-icons/bi'
 
 export function Searchbar(){
+  const searchModal = useSearchContext()
+
     return (
     <div className='w-full relative'>
       <input 
+        onChange={(e) => searchModal?.setSearchParams(e.target.value)}
         type="text" 
         placeholder="Procure por um de seus gateways" 
         className='w-full p-2 pr-10 rounded-md bg-transparent border border-zinc-400 text-zinc-400 placeholder:text-zinc-400 outline-none font-light text-xs whitespace-nowrap overflow-hidden text-ellipsis xl:text-lg'
--- src/components/gateway/GatewayGrid.tsx
+++ src/components/gateway/GatewayGrid.tsx
@@ -1,13 +1,35 @@
 'use client'
-import data from '@/services/api.json'
+import { useSearchContext } from '@/contexts/SearchContext';
 import { GatewayCard } from "./GatewayCard";
+import data from '@/services/api.json'
 
 export function GatewayGrid(){
+    const searchContext = useSearchContext();
+
     return (
       <main className='py-8 grid grid-cols-auto justify-items-center gap-6 xl:px-0'>
-        {data.gateways.map((gateway) => (
-          <GatewayCard key={gateway._id} name={gateway.name} devices={gateway.devices}/>
-          ))}
+        {searchContext?.searchParams ? (
+          data.gateways.filter((gateway) =>
+            gateway._id.includes(searchContext.searchParams) ||
+            gateway.name.includes(searchContext.searchParams)
+          )
+        .map((filteredGateways) => (
+          <GatewayCard
+            key={filteredGateways._id}
+            name={filteredGateways.name}
+            devices={filteredGateways.devices}
+          />
+            )
+          )
+        ) 
+        : 
+        data.gateways.map((gateway) => (
+          <GatewayCard 
+            key={gateway._id} 
+            name={gateway.name} 
+            devices={gateway.devices}
+          />
+        ))}
       </main>
         
     )
