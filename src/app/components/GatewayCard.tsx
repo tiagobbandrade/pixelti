9d2f01bff613f2ef1632613be9c40aa1fe1a1080
tiagobbandrade <tiagobbandrade2704@gmail.com>
2023-10-09 22:34:18 -0300

    refactor: change to responsivity sizes

--- src/app/components/GatewayCard.tsx
+++ src/app/components/GatewayCard.tsx
@@ -8,13 +8,13 @@ type GatewayCardT = {
 
 export function GatewayCard({gatewayName}: GatewayCardT){
     return (
-        <div className="max-w-[220px] px-9 py-11 border border-zinc-400 shadow-sm shadow-zinc-400 rounded-2xl flex flex-col items-center justify-center gap-3">
+        <div className="w-full px-9 py-11 border shadow-sm rounded-2xl flex flex-col items-center justify-center gap-3 border-zinc-400 shadow-zinc-400">
             <Image src={gatewayImage} alt="Imagem ilustrando um gateway"/>
-            <hr className='max-w-[148px] w-full bg-zinc-300 mt-6'/>
+            <hr className='w-3/5 bg-zinc-300 mt-6'/>
             <span className='text-lg font-semibold uppercase'>
                 {gatewayName}
             </span>
-            <button className='px-[10px] py-[6px] rounded-md text-xs font-normal flex items-center gap-2 mt-1 bg-[#BFCF00] text-[#F4F4F4]'>
+            <button className='px-[10px] py-2 max-w-[128px] w-full rounded-md text-xs font-normal flex items-center justify-center gap-2 mt-1 bg-[#BFCF00] text-[#F4F4F4]'>
                 Ver detalhes
                 <BsArrowRight className='w-4 h-4'/>
             </button>
