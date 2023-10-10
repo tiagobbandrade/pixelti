a9076269e15bed6fb4a31aa59d337ad4e8e9d389
tiagobbandrade <tiagobbandrade2704@gmail.com>
2023-10-09 23:07:48 -0300

    feat: start to add responsivity

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -2,11 +2,20 @@ import { FiLogOut } from 'react-icons/fi'
 import Image from "next/image";
 import PixeltiLogo from '../../../public/pixelti-logo.svg'
 import AvatarImage from '../../../public/profile-avatar.png'
+import { BiLeftArrow } from 'react-icons/bi';
 
-export function Sidebar(){
+export function Sidebar({onClick}: {onClick?: () => void}){
     return(
-        <nav className="fixed top-0 left-0 max-w-[232px] h-full w-full py-8 px-6 flex flex-col items-center justify-between bg-zinc-900">
+        <nav className="fixed top-0 left-0 max-w-[232px] h-screen w-full py-8 px-6 flex flex-col items-center justify-between bg-zinc-900 z-10
+            xl:sticky xl:max-w-xs"
+        >
             <Image src={PixeltiLogo} alt="PixelTi Logo"/>
+            <button 
+                onClick={onClick} 
+                className='bg-transparent border-none text-zinc-50 self-end -mr-2 xl:hidden'
+            >
+                <BiLeftArrow className="w-5 h-5"/>
+            </button>
             <div className="flex items-center justify-center flex-col gap-1">
                 <Image 
                     src={AvatarImage} 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,28 @@
+// 'use client'
 import { BiMenu } from 'react-icons/bi'
 import { Searchbar } from './components/Searchbar'
 import { GatewayCard } from './components/GatewayCard'
 import { Sidebar } from './components/Sidebar'
 
 export default function Home() {
+
   return (
-    <div className="px-8 py-6 relative">
-      <header className="flex items-center justify-center w-full gap-4">
-        <button className='p-1 border-none outline-none bg-transparent'>
-          <BiMenu className='w-9 h-9'/>
-        </button>
-          <Searchbar />
-      </header>
-      {/* <Sidebar /> */}
-      <main className='py-8 px-16 grid grid-cols-auto justify-items-center gap-4'>
-        <GatewayCard gatewayName='Gateway 1' />
-        <GatewayCard gatewayName='Gateway 2' />
-        <GatewayCard gatewayName='Gateway 3' />
-        <GatewayCard gatewayName='Gateway 4' />
-      </main>
+    <div className="px-8 py-6 relative xl:px-0 xl:py-0 xl:flex">
+      <Sidebar />
+      <div className='flex flex-col w-full p-8'>
+        <header className="flex items-center justify-center w-full gap-4">
+          <button className='p-1 border-none outline-none bg-transparent xl:hidden'>
+            <BiMenu className='w-9 h-9'/>
+          </button>
+            <Searchbar />
+        </header>
+        <main className='py-8 px-16 grid grid-cols-auto justify-items-center gap-6 xl:px-0'>
+          <GatewayCard gatewayName='Gateway 1' />
+          <GatewayCard gatewayName='Gateway 2' />
+          <GatewayCard gatewayName='Gateway 3' />
+          <GatewayCard gatewayName='Gateway 4' />
+        </main>
+      </div>
     </div>
   )
 }
