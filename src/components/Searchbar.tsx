f2fa6d1a41c45951579ce52b19e8f6e895f5abf9
tiagobbandrade <tiagobbandrade2704@gmail.com>
2023-10-10 14:41:26 -0300

    chore: chenge colors in searchbar

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -6,10 +6,10 @@ export function Searchbar(){
       <input 
         type="text" 
         placeholder="Procure por um de seus gateways" 
-        className='w-full p-2 pr-10 rounded-md bg-transparent border border-zinc-500 font-light text-xs whitespace-nowrap overflow-hidden text-ellipsis xl:text-xl'
+        className='w-full p-2 pr-10 rounded-md bg-transparent border border-zinc-400 text-zinc-400 placeholder:text-zinc-400 outline-none font-light text-xs whitespace-nowrap overflow-hidden text-ellipsis xl:text-lg'
       />
       <button className='border-none outline-none bg-transparent'>
-        <BiSearch className="absolute top-1/2 -translate-y-1/2 right-2 text-zinc-500"/>
+        <BiSearch className="absolute top-1/2 -translate-y-1/2 right-2 text-zinc-400"/>
       </button>
     </div>
     )
