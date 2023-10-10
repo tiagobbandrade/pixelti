32343878c272db9863a95f5da181c777d0652bd5
tiagobbandrade <tiagobbandrade2704@gmail.com>
2023-10-09 21:16:01 -0300

    feat: create a header mobile component

---
+++ src/app/components/Searchbar.tsx
@@ -0,0 +1,16 @@
+import { BiSearch } from 'react-icons/bi'
+
+export function Searchbar(){
+    return (
+    <div className='w-full relative'>
+      <input 
+        type="text" 
+        placeholder="Procure por um de seus gateways" 
+        className='w-full p-2 pr-10 rounded-md bg-transparent border border-zinc-500 font-light text-xs whitespace-nowrap overflow-hidden text-ellipsis'
+      />
+      <button className='border-none outline-none bg-transparent'>
+        <BiSearch className="absolute top-1/2 -translate-y-1/2 right-2 text-zinc-500"/>
+      </button>
+    </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import { BiMenu } from 'react-icons/bi'
+import { Searchbar } from './components/Searchbar'
+
 export default function Home() {
-  return <h1>Hello World!</h1>
+  return (
+    <div className="px-8 py-6">
+      <header className="flex items-center justify-center w-full gap-4">
+        <button className='p-1 border-none outline-none bg-transparent'>
+          <BiMenu className='w-9 h-9'/>
+        </button>
+          <Searchbar />
+      </header>
+    </div>
+  )
 }
