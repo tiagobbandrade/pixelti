802e082aa4e83e5761142f25a9ca4877c0701482
tiagobbandrade <tiagobbandrade2704@gmail.com>
2023-10-10 14:41:54 -0300

    chore: remove a padding

--- src/components/gateway/GatewayGrid.tsx
+++ src/components/gateway/GatewayGrid.tsx
@@ -4,7 +4,7 @@ import { GatewayCard } from "./GatewayCard";
 
 export function GatewayGrid(){
     return (
-      <main className='py-8 px-16 grid grid-cols-auto justify-items-center gap-6 xl:px-0'>
+      <main className='py-8 grid grid-cols-auto justify-items-center gap-6 xl:px-0'>
         {data.gateways.map((gateway) => (
           <GatewayCard key={gateway._id} name={gateway.name} devices={gateway.devices}/>
           ))}
